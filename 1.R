81d23bfc3c81e25870ede1a63095e0ecba3e0e3b
Xiaona Hu <Xiaona.Hu@UGent.be>
2017-09-15 10:24:35 +0200

    script to learn r

---
+++ 1.R
@@ -0,0 +1,52 @@
+### 
+1 + 100
+1 + 100
+1 +
+3 +5 *2
+# this is
+2e5
+2/10000
+?log
+1 == 1
+1 != 2
+?all.equal
+x <- 100
+x <- 1
+x <- x+1
+x
+fm.ruben <- 42
+fm.swc <- 41
+fm swc <- 42
+1:5
+2^(1:5)
+x <- 1:5
+fm.ruben <- 42
+ls()
+ls(all.names = )
+rm(x)
+rm(list <- ls())
+rm(list)
+sessionInfo()
+sessionInfo()
+install.packages("ggplot2")
+.mass <- 2
+_age <- 
+ min-length <- 
+min-length <- 2
+2wid <- 2
+install.packages("plyr")
+install.packages("gapminder")
+?log
+help(log)
+?"<-"
+library("ggplot2")
+vignette("ggplot2-specs")
+??log
+c(1, 2, 3)
+c('d', 'e','f')
+c(1, 2, 'f')
+mmimjfijsa <- c(1, 2, 'f')
+class(mmimjfijsa)
+is vector(mmimjfijsa)
+is.vector(mmimjfijsa)
+11
